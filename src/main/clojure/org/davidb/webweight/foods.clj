e4763e9322fd990885b475466c1e9b3d71952b5e
David Brown <clojure@davidb.org>
2009-11-15 20:11:28 -0800

    Use YAML instead of JSON for food database.
    This allows the food database to be shared with the Ruby code, and
    makes it overall easier to work with.
    
    Uses org.yaml.snakeyaml to load/parse the YAML.

--- src/main/clojure/org/davidb/webweight/foods.clj
+++ src/main/clojure/org/davidb/webweight/foods.clj
@@ -1,12 +1,27 @@
 (ns org.davidb.webweight.foods
   (:require [org.davidb.webweight
              [base :as base]])
-  (:use [clojure.contrib.json [read :only [read-json]]])
-  (:use [clojure.walk :only [keywordize-keys]]))
+  (:use [clojure.walk :only [keywordize-keys walk]])
+  (:import [org.yaml.snakeyaml Yaml])
+  (:import [java.util Map]))
+
+(defn deep-mapify
+  "Convert a Java map that isn't a clojure map into a clojure map.
+  Deeply converts the fields of the map as well."
+  [form]
+  (let [form (if (and (not (map? form))
+                      (instance? Map form))
+               (into {} form)
+               form)]
+    (walk deep-mapify identity form)))
 
 (defn load-foods
-  ([] (load-foods (str base/root "/foods.json")))
+  "Load the food database from the YAML store."
+  ([] (load-foods (str base/root "/foods.yaml")))
   ([path]
-   (with-open [rd (java.io.FileReader. #^String path)
-               rd (java.io.PushbackReader. rd)]
-     (keywordize-keys (read-json rd)))))
+   (with-open [rd (java.io.FileReader. #^String path)]
+     (let [yaml (Yaml.)
+           data (.load yaml rd)
+           data (deep-mapify data)
+           data (keywordize-keys data)]
+       data))))
